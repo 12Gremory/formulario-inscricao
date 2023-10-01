127ca2e0654afc08fe4db8e0e1eab4c9fd673d4f
Thomas <thomas0369@hotmail.com>
2023-10-01 10:46:51 -0300

    adição da validação de cpf

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Formulario from "./Formulario/Formulario";
 import "fontsource-roboto";
 
 function App() {
+  function aoEnviarForm(dados) {
+    return console.log(dados);
+  }
+
+  function validarCPF(cpf) {
+    if (cpf.length !== 11) {
+      return { valido: false, texto: "O CPF deve conter 11 dígitos" };
+    } else {
+      return { valido: true, texto: "" };
+    }
+  }
+
   return (
     <Container component="article" maxWidth="sm">
       <Typography variant="h3" component="h1" align="center">
         Formulário de Cadastro
       </Typography>
-      <Formulario />
+      <Formulario aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
     </Container>
   );
 }
--- src/Formulario/Formulario.js
+++ src/Formulario/Formulario.js
@@ -1,9 +1,26 @@
 import { Button, FormControlLabel, Switch, TextField } from "@mui/material";
+import { useState } from "react";
 
-function Formulario() {
+function Formulario({ aoEnviar, validarCPF }) {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [promocoes, setPromocoes] = useState(true);
+  const [novidades, setNovidades] = useState(true);
+  const [erro, setErro] = useState({ cpf: { valido: true, texto: "" } });
+
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        aoEnviar({ nome, sobrenome, cpf, promocoes, novidades });
+      }}
+    >
       <TextField
+        onChange={(e) => {
+          setNome(e.target.value);
+        }}
+        value={nome}
         id="Nome"
         label="Nome"
         variant="outlined"
@@ -11,6 +28,10 @@ function Formulario() {
         margin="normal"
       />
       <TextField
+        onChange={(e) => {
+          setSobrenome(e.target.value);
+        }}
+        value={sobrenome}
         id="Sobrenome"
         label="Sobrenome"
         variant="outlined"
@@ -18,6 +39,18 @@ function Formulario() {
         margin="normal"
       />
       <TextField
+        onChange={(e) => {
+          setCpf(e.target.value);
+        }}
+        onBlur={(e) => {
+          const valido = validarCPF(e.target.value)
+          setErro({
+            cpf: { valido },
+          });
+        }}
+        error={!erro.cpf.valido}
+        helperText={erro.cpf.texto}
+        value={cpf}
         id="Cpf"
         label="CPF"
         variant="outlined"
@@ -27,11 +60,29 @@ function Formulario() {
 
       <FormControlLabel
         label="Promoções"
-        control={<Switch name="Promoções" defaultChecked color="primary" />}
+        control={
+          <Switch
+            onChange={(e) => {
+              setPromocoes(e.target.checked);
+            }}
+            checked={promocoes}
+            name="Promoções"
+            color="primary"
+          />
+        }
       />
       <FormControlLabel
         label="Novidades"
-        control={<Switch name="Novidades" defaultChecked color="primary" />}
+        control={
+          <Switch
+            onChange={(e) => {
+              setNovidades(e.target.checked);
+            }}
+            checked={novidades}
+            name="Novidades"
+            color="primary"
+          />
+        }
       />
 
       <Button type="submit" variant="contained" color="primary">
